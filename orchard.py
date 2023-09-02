35c31f4a3909ae94b17a693c8010f65c69387c1f
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-09-02 19:27:47 +0100

    feat: add support for loading all datasets

--- orchard.py
+++ orchard.py
@@ -1,5 +1,5 @@
+import numpy as np
 import pandas as pd
-import dask_cudf as cd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
@@ -8,17 +8,42 @@ import glob
 from datetime import datetime
 
 
-# See https://help.ceda.ac.uk/article/4982-midas-open-user-guide
+# See https://help.ceda.ac.uk/article/4982-midas-open-user-guide/
 QC_VERSION = 1
 DATASET_VERSION = 202207
-DATASETS = ["uk-hourly-rain-obs", "uk-hourly-weather-obs"]
+DATASETS = {
+    "uk-hourly-weather-obs": {
+        "index": "ob_time",
+        "skiprows": 280,
+        "columns": [
+            "ob_time",
+            "wind_direction",
+            "wind_speed",
+            "air_temperature",
+            "rltv_hum",
+            "msl_pressure",
+            "stn_pres",
+            "alt_pres",
+        ],
+    },
+    "uk-hourly-rain-obs": {
+        "index": "ob_end_time",
+        "skiprows": 61,
+        "columns": ["ob_end_time", "ob_hour_count", "prcp_amt", "prcp_dur"],
+    },
+    "uk-soil-temperature-obs": {
+        "index": "ob_time",
+        "skiprows": 85,
+        "columns": ["ob_time", "q5cm_soil_temp", "q10cm_soil_temp"],
+    },
+}
 
 
-def getCounties():
+def get_counties(dataset="uk-hourly-weather-obs"):
     counties = {}
 
     paths = glob.glob(
-        f"/home/sam/ukmo-midas-open/data/uk-hourly-weather-obs/dataset-version-{DATASET_VERSION}/*",
+        f"/home/sam/ukmo-midas-open/data/{dataset}/dataset-version-{DATASET_VERSION}/*",
         recursive=True,
     )
 
@@ -28,9 +53,37 @@ def getCounties():
     return counties
 
 
-def getCountyStations(county):
+def get_stations_metadata():
+    cols = [
+        "historic_county",
+        "station_latitude",
+        "station_longitude",
+        "station_elevation",
+    ]
+    metadata = []
+
+    for dataset in DATASETS:
+        df = pd.read_csv(
+            f"/home/sam/ukmo-midas-open/data/{dataset}/midas-open_{dataset}_dv-{DATASET_VERSION}_station-metadata.csv",
+            skiprows=46,
+            usecols=["station_file_name"] + cols,
+        )
+
+        df.drop(df.tail(1).index, inplace=True)
+        df[dataset] = 1
+
+        metadata.append(df)
+
+    return (
+        pd.concat(metadata)
+        .groupby("station_file_name")
+        .aggregate({c: "first" for c in cols} | {d: "sum" for d in DATASETS})
+    )
+
+
+def get_county_stations(dataset, county):
     stations = {}
-    paths = glob.glob(f"{getCounties()[county]}/*")
+    paths = glob.glob(f"{get_counties(dataset)[county]}/*")
 
     for path in paths:
         stations[os.path.split(path)[-1]] = path
@@ -38,58 +91,54 @@ def getCountyStations(county):
     return stations
 
 
-def getStationCSVFiles(county, station):
+def get_station_csv_files(dataset, county, station, qc=QC_VERSION):
     return sorted(
-        glob.glob(f"{getCounties()[county]}/{station}/qc-version-{QC_VERSION}/*.csv")
+        glob.glob(f"{get_counties(dataset)[county]}/{station}/qc-version-{qc}/*.csv")
     )
 
 
-def getStationParquetFile(county, station):
-    return f"{getCounties()[county]}/{station}/{station}.parquet"
+def get_station_parquet_file(dataset, county, station):
+    return f"{get_counties(dataset)[county]}/{station}/{station}.parquet"
 
 
-def getStationFilePath(county, station):
-    return f"{getCounties()[county]}/{station}/"
+def get_station_file_path(dataset, county, station):
+    return f"{get_counties(dataset)[county]}/{station}/"
 
 
-def readAllData():
+def load_dataset(dataset):
     data = []
-    for county in getCounties():
-        for station in getCountyStations(county):
-            station_data = cd.read_parquet(getStationParquetFile(county, station))
+    for county in get_counties(dataset):
+        for station in get_county_stations(dataset, county):
+            station_data = pd.read_parquet(
+                get_station_parquet_file(dataset, county, station)
+            )
             station_data["county"] = county
             station_data["station"] = station
             data.append(station_data)
-    return cd.concat(data)
+    return pd.concat(data)
 
 
-def convertStationData(verbose=False):
-    for county in getCounties():
-        for station in getCountyStations(county):
+def convert_data(dataset, verbose=False):
+    for county in get_counties(dataset):
+        for station in get_county_stations(dataset, county):
             start_time = datetime.now()
             station_data = []
-            for file in getStationCSVFiles(county, station):
+
+            # First try loading all CSV files from default QC_VERSION directory
+            # If no files, try the other QC_VERSION folder
+            csv_files = get_station_csv_files(
+                dataset, county, station
+            ) or get_station_csv_files(dataset, county, station, 1 - QC_VERSION)
+
+            for file in csv_files:
                 df = pd.read_csv(
                     file,
-                    index_col="ob_time",
-                    parse_dates=["ob_time"],
                     engine="c",
-                    skiprows=280,
                     on_bad_lines="warn",
-                    usecols=[
-                        "ob_time",
-                        "wind_direction",
-                        "wind_speed",
-                        "visibility",
-                        "msl_pressure",
-                        "vert_vsby",
-                        "air_temperature",
-                        "dewpoint",
-                        "wetb_temp",
-                        "rltv_hum",
-                        "stn_pres",
-                        "alt_pres",
-                    ],
+                    index_col=DATASETS[dataset]["index"],
+                    parse_dates=[DATASETS[dataset]["index"]],
+                    skiprows=DATASETS[dataset]["skiprows"],
+                    usecols=DATASETS[dataset]["columns"],
                 )
                 # Remove last row "end data"
                 df.drop(df.tail(1).index, inplace=True)
@@ -98,7 +147,8 @@ def convertStationData(verbose=False):
             station_df = pd.concat(station_data)
             df_pa = pa.Table.from_pandas(station_df)
             pq.write_table(
-                df_pa, getStationFilePath(county, station) + station + ".parquet"
+                df_pa,
+                get_station_file_path(dataset, county, station) + station + ".parquet",
             )
 
             if verbose:
@@ -107,8 +157,20 @@ def convertStationData(verbose=False):
                     f"in {datetime.now() - start_time} seconds",
                 )
 
-    print("All data converted to parquet")
+    print(f"All data converted to parquet for {dataset}")
 
 
-def clean_data(df):
-    return df
+def haversine_distance(lat1, lon1, lat2, lon2):
+    EARTH_RADIUS_KM = 6371
+    lat1, lon1, lat2, lon2 = map(np.radians, [lat1, lon1, lat2, lon2])
+
+    return (
+        2
+        * EARTH_RADIUS_KM
+        * np.arcsin(
+            np.sqrt(
+                (np.sin((lat2 - lat1) / 2) ** 2)
+                + (np.cos(lat1) * np.cos(lat2) * (np.sin((lon2 - lon1) / 2) ** 2))
+            )
+        )
+    )
